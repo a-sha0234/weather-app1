839ac768a7bdf5d92e6aec6bb827c74cf25c1c73
a-sha0234 <asharma_31@hotmail.com>
2021-11-23 21:55:13 +0000

    js: create function extract data from api call

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const submitButton = document.querySelector(".submit");
+let nameOfCity = "";
 
 async function getApi(cityname) {
   //function to fetch data and return it
@@ -10,16 +11,48 @@ async function getApi(cityname) {
       `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=708dbd9b677bb41f1461a55259144588`
     );
     const response = await data.json();
+
     console.log(response);
+    return [response.name, response.main.temp_max];
   } catch (err) {
     console.log("There was an error fetching the data: " + err);
   }
 }
 
+function extractData() {
+  //function to extract data and put into an array
+  let arrApiData = [];
+  getApi(nameOfCity).then((x) => {
+    arrApiData.push(x);
+  });
+  return arrApiData;
+}
+
+async function getCityName() {
+  //function that gets city name
+  const city = arrdata.name;
+  console.log(city);
+  return city;
+}
+
+function maxTemp() {
+  //   let h = getApi(nameOfCity).then((x) => {
+  //     console.log(x[1]);
+  //   });
+  //   return h;
+}
+
 function main() {
   submitButton.addEventListener("click", function () {
     let cityName = document.querySelector("#cityname").value;
-    getApi(cityName);
+    nameOfCity += cityName;
+    // console.log(
+    //   getApi(nameOfCity).then((x) => {
+    //     console.log(x[1]);
+    //   })
+    // );
+    console.log(extractData());
+    nameOfCity = "";
   });
 }
 
